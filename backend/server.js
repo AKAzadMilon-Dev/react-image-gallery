109680bcc48f04752898435ce48cee69ad3e5f56
milon2267 <gdazad2267@gmail.com>
2022-07-03 19:40:54 +0600

    file setup update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send("I am running to server port ")
+    res.send("Hello World")
 })
 
 app.listen(8000)
